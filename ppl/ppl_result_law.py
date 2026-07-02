6860e98b97252b903383e8e87e2bd3cd3b89d4c6
xuxiaofeng0411 <xuxiaofeng@st.usst.edu.cn>
2026-07-02 13:37:11 +0800

    Update ppl_result_law.py

--- ppl/ppl_result_law.py
+++ ppl/ppl_result_law.py
@@ -1,9 +1,7 @@
 import ppl_result_pubmed as runner
 
-
 runner.DOMAIN = "law"
 runner.OUTPUT_JSON = "/root/autodl-tmp/project12/outputs/ppl_result_law.json"
-
 runner.MODEL_SPECS = [
     ("original", "/root/autodl-tmp/models/facebook/opt-125m"),
     ("law_ft_stage1", "/root/autodl-tmp/models/law/opt-125m-mark1-nomark-ft-law"),
@@ -14,13 +12,11 @@ runner.MODEL_SPECS = [
     ("law_watermark_stage3", "/root/autodl-tmp/models/facebook/law/opt-125m-mark3-nomark"),
     ("law_watermark_stage4", "/root/autodl-tmp/models/facebook/law/opt-125m-mark4-nomark"),
 ]
-
 runner.DOMAIN_DATASETS = [
     ("law_stage1", "/root/autodl-tmp/datasets/processed_law1_35k"),
     ("law_stage2", "/root/autodl-tmp/datasets/processed_law2_35k"),
     ("law_stage3", "/root/autodl-tmp/datasets/processed_law3_35k"),
 ]
-
 
 if __name__ == "__main__":
     runner.main()
